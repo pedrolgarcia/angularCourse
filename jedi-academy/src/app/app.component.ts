b75aade4da0c212264f95554fa84b253607a202a
Pedro Garcia <pedrolopes.info@gmail.com>
2018-10-13 01:49:44 -0300

    fim fundamentos

--- jedi-academy/src/app/app.component.ts
+++ jedi-academy/src/app/app.component.ts
@@ -1,10 +1,24 @@
+import { Student } from './student/student.model';
 import { Component } from '@angular/core';
 
-@Component({
-  selector: 'jad-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+@Component({ // decorator de componente em angular
+  selector: 'jad-root', // seletor que referencia o componente
+  templateUrl: './app.component.html' // template do o componente
+  // styleUrls: ['./app.component.css'] // estilo do componente
+  // template: '<h1>Hello</h1>' // template inline
+                /* `<h1>
+                      Hello
+                    </h1>` */ 
 })
-export class AppComponent {
-  title = 'jedi-academy';
+export class AppComponent { // classe do componente
+
+  students: Student[] = [
+    { name: 'Luke', isJedi: true, temple: 'Coruscant' },
+    { name: 'Han Solo', isJedi: false },
+    { name: 'Leia', isJedi: false }
+  ]
+
+  // luke = { name: 'Luke', isJedi: true, temple: 'Coruscant' }
+  // han = { name: 'Han Solo', isJedi: false }
+  // leia = { name: 'Leia', isJedi: false }
 }
