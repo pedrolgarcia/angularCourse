9070dcf7e0418641438e18a6ed93ad3b2b65df62
Pedro <pedrolopes.info@gmail.com>
2018-10-16 01:18:25 -0300

    implementando menu-item do meat

--- meat-app/src/app/restaurants/restaurants.service.ts
+++ meat-app/src/app/restaurants/restaurants.service.ts
@@ -9,6 +9,7 @@ import "rxjs/add/operator/catch";
 
 import { MEAT_API } from "app/app.api";
 import { ErrorHandler } from "app/app.error-handler";
+import { MenuItem } from "app/restaurant-detail/menu-item/menu-item.model";
 
 @Injectable()
 export class RestaurantsService {
@@ -17,6 +18,24 @@ export class RestaurantsService {
 
     restaurants(): Observable<Restaurant[]> {
         return this.http.get(`${MEAT_API}/restaurants`)
+            .map(response => response.json())
+            .catch(ErrorHandler.handleError)
+    }
+
+    restaurantById(id: string): Observable<Restaurant> {
+        return this.http.get(`${MEAT_API}/restaurants/${id}`)
+            .map(response => response.json())
+            .catch(ErrorHandler.handleError)
+    }
+
+    reviewsOfRestaurant(id: string): Observable<any> {
+        return this.http.get(`${MEAT_API}/restaurants/${id}/reviews`)
+            .map(response => response.json())
+            .catch(ErrorHandler.handleError)
+    }
+
+    menuOfRestaurant(id: string): Observable<MenuItem[]> {
+        return this.http.get(`${MEAT_API}/restaurants/${id}/menu`)
             .map(response => response.json())
             .catch(ErrorHandler.handleError)
     }
